55b89d4a6af0fec28b28ed621d7ea9d207d6cdc9
amansadhwani <amansadhwani@gmail.com>
2022-03-12 19:32:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Home from './pages/Home';
+import Coins from './pages/Coins';
+import { makeStyles } from "@material-ui/core";
 
-function App() {
+const App = () => {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/coins/:id" element={<Coins />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter} from 'react-router-dom'
+import CryptoContext from './CryptoContext';
+import "react-alice-carousel/lib/alice-carousel.css";
 ReactDOM.render(
-  <React.StrictMode>
+    <BrowserRouter>
+    <CryptoContext>
     <App />
-  </React.StrictMode>,
+    </CryptoContext>
+    </BrowserRouter>
+ ,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
